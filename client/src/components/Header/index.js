75775034d8ee761e5788ad09030d6b22d46c980d
Brian Farmer <brianlfarmerllc@gmail.com>
2020-07-11 16:09:25 -0500

    added background styling to html body

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -5,10 +5,10 @@ import "./header.css"
 function Header() {
     return (
         <header>
-            <div className="content">
+            <div className="container-fluid content">
                 <h1>Don't Forget to <span>README</span></h1>
                 <h3>Clean and Consistent Documentation</h3>
-                <p>Keep your documentation styling consistent across all your projects automatically with this README.md formatter. 
+                <p>Keep your documentation styling consistent across all your projects simply with this README.md formatter. 
                     Its simple to use and feel free to taylor it to your liking. Check out the Repo below for more information. </p>
                 <br />
                 

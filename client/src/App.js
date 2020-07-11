6412ca680b6ba3e99d2a40767109fb52015a4deb
Brian Farmer <brianlfarmerllc@gmail.com>
2020-07-11 14:01:03 -0500

    set up basic express server

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-  
+      <h1 style={{textAlign:"center"}}>Testing Application</h1>
     </div>
   );
 }
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+// const routes = require("./routes");
+
+const PORT = process.env.PORT || 3001;
+
+// Define middleware here
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+// Add routes, both API and view
+// app.use(routes);
+
+
+// Start the API server
+app.listen(PORT, () => {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});

c2544156daa6ef6dfec27873238e16ae75cce908
Brian Farmer <brianlfarmerllc@gmail.com>
2020-07-11 15:43:24 -0500

    create header

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./header.css"
+
+
+function Header() {
+    return (
+        <header>
+            <div className="content">
+                <h1>Don't Forget to <span>README</span></h1>
+                <h3>Clean and Consistent Documentation</h3>
+                <p>Keep your documentation styling consistent across all your projects automatically with this README.md formatter. 
+                    Its simple to use and feel free to taylor it to your liking. Check out the Repo below for more information. </p>
+                <br />
+                
+                <a className="github" href="https://www.github.com/brianlfarmerllc/" target="blank"><i
+                  className="fa fa-github"></i></a>
+            </div>
+        </header>
+    )
+}
+
+export default Header

6abdc0854b930342ed931e2052bb6c5778e79f6b
Brian Farmer <brianlfarmerllc@gmail.com>
2020-07-15 14:20:37 -0500

    added webpack and updated icons

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -3,16 +3,18 @@ import "./form.css"
 
 function Form(props) {
     return (
-        <div className="row justify-content-center" style={{marginBottom:"0"}}>
-            <div className=" col-11 col-xl-8">
-                <div className="form">
-                    <div className="form-header">
-                        <h2>readme generator</h2>
-                    </div>
-                    <div className="form-content">
-                        <form>
-                            {props.children}
-                        </form>
+        <div className="container-fluid">
+            <div className="row justify-content-center" style={{ marginBottom: "0" }}>
+                <div className=" col-11 col-xl-8">
+                    <div className="form">
+                        <div className="form-header">
+                            <h2>readme generator</h2>
+                        </div>
+                        <div className="form-content">
+                            <form>
+                                {props.children}
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
